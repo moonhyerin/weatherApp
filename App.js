d98abffd9095b069e74dccca4ac01e3144e4eca0
unknown <hrmoon081632@gmail.com>
2022-08-24 17:38:22 +0900

    Learn about css, components

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hallo~</Text>
+      <Text style={styles.text}>Hallo~</Text>
+      {/* 운영체제와 소통하는 컴포넌트인 StatusBar, 실제 화면에 렌더링되지 않는다! */}
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 48,
   },
 });
